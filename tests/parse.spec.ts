9665a7aecc28696ee8526e3e2deb953d98c562a6
Devon Herlitz <devonherlitz@gmail.com>
2018-12-29 19:15:38 -0500

    Added awaits so that tests won't finish before expect() is called

--- tests/parse.spec.ts
+++ tests/parse.spec.ts
@@ -2,7 +2,7 @@ import { Kweery as k } from "../src/kweery";
 import { Base } from "../src/forms/forms";
 
 function expectErr(prom: Promise<Base>) {
-  prom.catch(e => {
+  return prom.catch(e => {
     expect(e.length).toBeTruthy();
   });
 }
@@ -19,33 +19,34 @@ test("successful parse", async () => {
 
 test("failed parse: missing operator", async () => {
   expect.assertions(1);
-  expectErr(k.parse("t.name = 'Sam' t.age > 19"));
+  await expectErr(k.parse("t.name = 'Sam' t.age > 19"));
 });
 
 test("failed parse: missing accessor part", async () => {
   expect.assertions(1);
-  expectErr(k.parse("t. = 'Sam'"));
+  await expectErr(k.parse("t. = 'Sam'"));
 });
 
 test("failed parse: missing expression rhs", async () => {
   expect.assertions(1);
-  expectErr(k.parse("t.name ="));
+  await expectErr(k.parse("t.name ="));
 });
 
 test("failed parse: missing expression rhs with valid following expression", async () => {
   expect.assertions(1);
-  expectErr(k.parse("t.name = and t.age < 19"));
+  await expectErr(k.parse("t.name = and t.age < 19"));
 });
 
 test("failed parse: single tokens", async () => {
   let tokens = ["and", "or", "=", "<", ">", "<=", ">=", "t.apple", "t", "t.", ".t"];
   expect.assertions(tokens.length);
-  tokens.forEach(token => {
-    expectErr(k.parse(token));
-  });
+  Promise.all(
+    tokens.map(token => {
+      expectErr(k.parse(token));
+    }));
 });
 
 test("failed parse: double quotes instead of single", async () => {
   expect.assertions(1);
-  expectErr(k.parse("t.name = \"Sam\" and t.age < 19"));
+  await expectErr(k.parse("t.name = \"Sam\" and t.age < 19"));
 });
