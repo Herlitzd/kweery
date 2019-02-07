5ee626921949b0bb910e476f709234e3c47f2d89
Devon Herlitz <devonherlitz@gmail.com>
2019-02-07 16:03:46 -0500

    Basic OData filters can be generated, basic tests, still not sure if this is worthwhile at all

--- src/forms/logical.ts
+++ src/forms/logical.ts
@@ -1,15 +1,19 @@
 import { SetWiseOperator, UnaryOperator } from "./forms";
+import { odata } from "../plugins/plugin_attribute";
 
+@odata("or")
 export class OrOperator extends SetWiseOperator {
   get rawOperator(): (l: Boolean, r: Boolean) => Boolean {
     return (l: Boolean, r: Boolean) => l || r;
   }
 }
+@odata("and")
 export class AndOperator extends SetWiseOperator {
   get rawOperator(): (l: Boolean, r: Boolean) => Boolean {
     return (l: Boolean, r: Boolean) => l && r;
   }
 }
+@odata("not")
 export class NotOperator extends UnaryOperator {
   apply(env: Map<String, any>): Boolean {
     return !this.param.apply(env);
